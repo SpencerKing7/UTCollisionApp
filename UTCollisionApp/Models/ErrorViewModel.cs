8cdaa426eec384a17e44ab77b12640b42902f296
SpencerKing7 <spencerking.c@gmail.com>
2022-04-04 08:58:15 -0600

    Add project files.

---
+++ UTCollisionApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UTCollisionApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
