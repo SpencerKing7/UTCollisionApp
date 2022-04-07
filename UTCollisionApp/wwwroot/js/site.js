09870ecb763c6efaaefd13bde692260bde6d3bb2
ARCDave <davidpm.meyers@gmail.com>
2022-04-07 12:47:03 -0600

    User Crud done

--- UTCollisionApp/wwwroot/js/site.js
+++ UTCollisionApp/wwwroot/js/site.js
@@ -5,3 +5,16 @@
 function removeDiv() {
     document.getElementById("cookie").style.display = "none";
 }
+
+function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
