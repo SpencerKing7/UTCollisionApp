923d097be4dc62024020ff212052213de921bf17
kbruner4 <90485716+kbruner4@users.noreply.github.com>
2022-04-07 12:08:22 -0600

    part of cookie notification

--- UTCollisionApp/wwwroot/js/site.js
+++ UTCollisionApp/wwwroot/js/site.js
@@ -5,3 +5,25 @@
 function removeDiv() {
     document.getElementById("cookie").style.display = "none";
 }
+
+$('.close-div').click(function () {
+    $(this).parent().parent().remove();
+});
+
+function supports_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+if (supports_storage()) localStorage['sitename.nofullscreen'] = true;
+
+if (localStorage['sitename.nofullscreen'] == "true") {
+    //go right to #page
+}
+
+$(document).ready(function () {
+    if (localStorage['nofullscreen'] == 'true') $('.fullscreen').hide();
+});
