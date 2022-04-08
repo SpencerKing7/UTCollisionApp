b610e6f75ea7abc1864871f6d7cc31c278bc20f6
ARCDave <davidpm.meyers@gmail.com>
2022-04-08 01:15:04 -0600

    Who knows

--- UTCollisionApp/Startup.cs
+++ UTCollisionApp/Startup.cs
@@ -155,12 +155,12 @@ namespace UTCollisionApp
             app.UseAuthentication();
             app.UseAuthorization();
 
-            //app.Use(async (ctx, next) =>
-            //{
-            //    ctx.Response.Headers.Add("Content-Security-Policy",
-            //                            "default-src 'self'");
-            //    await next();
-            //});
+            app.Use(async (ctx, next) =>
+            {
+                ctx.Response.Headers.Add("Content-Security-Policy",
+                                        "default-src 'self'");
+                await next();
+            });
 
             app.UseEndpoints(endpoints =>
             {
